54219fb26ef0a84209b21f994c1a6afda8e50fc6
brianhcliao <brianhcliao@gmail.com>
2019-01-23 14:08:37 +0800

    Revised Utils

--- scenarios/alphabot/utility/connect.m
+++ scenarios/alphabot/utility/connect.m
@@ -1,10 +1,29 @@
-mypi = raspi('192.168.10.108', 'pi', 'arthur');
-writeDigitalPin(mypi, 6, 1);
-writeDigitalPin(mypi, 26, 1);
+% Connect to Raspberry Pi board
+try
+    disp('Connecting to Arthur...');
+    mypi = raspi('192.168.10.108', 'pi', 'arthur');
+    disp('"mypi" assigned to Arthur');
+catch ME
+    disp(getReport(ME, 'extended', 'hyperlinks', 'on'));
+    try
+        disp('Connecting to Davis...');
+        mypi = raspi('192.168.10.109','pi','davis');
+        disp('"mypi" assigned to Davis');
+    catch ME
+        disp(getReport(ME, 'extended', 'hyperlinks', 'on'));
+    end
+end
+
+% Pin configuration and motor enabling
 configurePin(mypi, 13, 'PWM');
 configurePin(mypi, 20, 'PWM');
 configurePin(mypi, 12, 'PWM');
 configurePin(mypi, 21, 'PWM');
+configurePin(mypi, 6, 'DigitalOutput');
+configurePin(mypi, 26, 'DigitalOutput');
+writeDigitalPin(mypi, 6, 1);
+writeDigitalPin(mypi, 26, 1);
+
 
 % 13, 21
 
